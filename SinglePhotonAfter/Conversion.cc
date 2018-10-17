e25e25bcef250b79534e7461e993588923c5f34a
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2018-10-17 15:39:17 -0600

    made conversion class

---
+++ SinglePhotonAfter/Conversion.cc
@@ -0,0 +1,24 @@
+#include "Conversion.h"
+
+#include <g4eval/SvtxEvaluator.h>
+
+int Conversion::setRecoTracks(SvtxTrackEval* trackeval){	
+  if (e1)
+  {
+  	reco1=trackeval->best_track_from(e1); // have not checked that these are in range 
+  }
+  if (e2)
+  {
+  	reco2=trackeval->best_track_from(e12;
+  }
+  int r=0;
+  if (reco1)
+  {
+  	r++;
+  }
+  if (reco2)
+  {
+  	r++;
+  }
+  return r;
+}
---
+++ SinglePhotonAfter/Conversion.h
@@ -0,0 +1,72 @@
+#ifndef CONVERSION_H__
+#define CONVERSION_H__
+
+#include <g4main/PHG4Particle.h>
+#include <g4main/PHG4VtxPoint.h>
+#include <g4hough/SvtxTrack.h>
+
+class SvtxTrackEval;
+
+class Conversion
+{
+public:
+  Conversion(){}
+  Conversion(PHG4VtxPoint* vtx);
+  ~Conversion(){
+    //dont delete the points as you are not the owner and did not make your own copies
+  }
+  inline void setElectron(PHG4Particle* e){
+    if (e1)
+    {
+      if (e2)
+      {
+        std::cout<<"WARNING: oversetting conversion electrons"<<std::endl;
+      }
+      else{
+        e2=e;
+      }
+    }
+    else{
+      e1=e;
+    }
+  }
+  inline void setVtx(PHG4VtxPoint* vtx){
+    this->vtx=vtx;
+  }
+  inline PHG4VtxPoint* getVtx()const{
+    return vtx;
+  }
+  inline bool isComplete()const{
+    if (e1&&e2&&photon)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  inline bool hasPair()const{
+    if (e1&&e2)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  int setRecoTracks(SvtxTrackEval* trackeval);
+  /*bool acceptancePair(){
+
+  }*/
+private:
+  PHG4Particle* e1=NULL;
+  PHG4Particle* e2=NULL;
+  PHG4Particle* photon=NULL;
+  PHG4VtxPoint* vtx=NULL;
+  SvtxTrack* reco1=NULL;
+  SvtxTrack* reco2=NULL;
+  /*inline bool inAcceptance(){
+
+  }*/
+};
+#endif //CONVERSION_H__
