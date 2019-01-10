7bd3b423a8ced933a15a978b3129194756c8bff2
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-01-10 14:53:40 -0700

    found cluster bug and optimized loop

--- PhotonConversion/RecoConversionEval.cc
+++ PhotonConversion/RecoConversionEval.cc
@@ -35,24 +35,27 @@ int RecoConversionEval::process_event(PHCompositeNode *topNode) {
 	cout<<"Did pointers: \n \n";
   int bigLoopCount=0;
   for ( SvtxTrackMap::Iter iter = _allTracks->begin(); iter != _allTracks->end(); ++bigLoopCount) {
-    SvtxTrack* thisTrack = iter->second;
+  	if (bigLoopCount%1000==0)
+  	{
+  		cout<<bigLoopCount<<" tracks processed"<<endl;
+  	}
     //I want to now only check e tracks so check the clusters of the |charge|=1 tracks
-    if (abs(thisTrack->get_charge())==1)
+    if (abs(iter->second->get_charge())==1)
     {
+    	SvtxTrack* thisTrack = iter->second;
       RawCluster* bestCluster= _mainClusterContainer->getCluster(thisTrack->get_cal_cluster_id(SvtxTrack::CAL_LAYER(1)));
       //what if not cluster is found?
       if(bestCluster&&bestCluster->get_prob()>_kEMProbCut){
       	//loop over the following tracks
       	for (SvtxTrackMap::Iter jter = ++iter; jter != _allTracks->end(); ++jter)
       	{
-      		SvtxTrack* nextTrack = jter->second;
       		//check that the next track is an opposite charge electron
-      		if (thisTrack->get_charge()*-1==nextTrack->get_charge())
+      		if (thisTrack->get_charge()*-1==jter->second->get_charge())
       		{
-      			RawCluster* nextCluster= _mainClusterContainer->getCluster(thisTrack->get_cal_cluster_id(SvtxTrack::CAL_LAYER(1)));
+      			RawCluster* nextCluster= _mainClusterContainer->getCluster(jter->second->get_cal_cluster_id(SvtxTrack::CAL_LAYER(1)));
       			//what if not cluster is found?
       			if(nextCluster&&nextCluster->get_prob()>_kEMProbCut){
-      				pairCuts(thisTrack,nextTrack);
+      				pairCuts(thisTrack,jter->second);
       			}
       		}
       	}
