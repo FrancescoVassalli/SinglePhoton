bb437de658f0b1ecc1659963b9c731f7a81dacca
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-01-18 22:26:22 -0500

    track ROC

--- analysis/backgroundPlotter.C
+++ analysis/backgroundPlotter.C
@@ -26,6 +26,11 @@ TChain* handleFile(string name, string extension, string treename, unsigned int 
   return all;
 }
 
+float kPurityCutProb=.8; //for high purity make cluster_prob<.8
+float kEffCutProb=.9; //for high efficiency make cluster_prob<.9
+//int kPurityCutLayer=1; //for high purity make layer==1
+//int kEffCutLayer =1; //for high efficiency make layer <=1
+
 void backgroundPlotter()
 {
   int pid;
@@ -66,57 +71,66 @@ void backgroundPlotter()
   unsigned long pim=0;
   unsigned int p=0;
   unsigned int mu=0;
-
+  unsigned rejection=0;
   for (int event = 0; event < backTree->GetEntries(); ++event)
   {
     backTree->GetEvent(event);
-    switch(pid){
-    	case 211:
-    		h_pip_prob->Fill(cluster_prob);
-    		//counts->Fill(0);
-    		pip++;
-    		break;
-    	case -211:
-    		h_pim_prob->Fill(cluster_prob);
-    		//counts->Fill(1);
-    		pim++;
-    		break;
-    	case 2212:
-    		h_p_prob->Fill(cluster_prob);
-    		//counts->Fill(2);
-    		p++;
-    		break;
-    	case -2212:
-    		h_p_prob->Fill(cluster_prob);
-    		//counts->Fill(2);
-    		p++;
-    		break;
-    	case 13:
-    		h_mu_prob->Fill(cluster_prob);
-    		//counts->Fill(3);
-    		mu++;
-    		break;
-    	case -13:
-    		h_mu_prob->Fill(cluster_prob);
-    		//counts->Fill(3);
-    		mu++;
-    		break;
-    	default:
-    		break;
+    if (cluster_prob<kPurityCutProb)
+    {
+    	switch(pid){
+	    	case 211:
+	    		h_pip_prob->Fill(cluster_prob);
+	    		//counts->Fill(0);
+	    		pip++;
+	    		break;
+	    	case -211:
+	    		h_pim_prob->Fill(cluster_prob);
+	    		//counts->Fill(1);
+	    		pim++;
+	    		break;
+	    	case 2212:
+	    		h_p_prob->Fill(cluster_prob);
+	    		//counts->Fill(2);
+	    		p++;
+	    		break;
+	    	case -2212:
+	    		h_p_prob->Fill(cluster_prob);
+	    		//counts->Fill(2);
+	    		p++;
+	    		break;
+	    	case 13:
+	    		h_mu_prob->Fill(cluster_prob);
+	    		//counts->Fill(3);
+	    		mu++;
+	    		break;
+	    	case -13:
+	    		h_mu_prob->Fill(cluster_prob);
+	    		//counts->Fill(3);
+	    		mu++;
+	    		break;
+	    	default:
+	    		break;
+	    }
+	    b_deta->Fill(deta);
+	    b_layer->Fill(layer);
+    	rejection++;
     }
-    b_deta->Fill(deta);
-    b_layer->Fill(layer);
+    
   } 
   //counts->Scale(1./(float)backTree->GetEntries());
   float total=backTree->GetEntries();
   std::cout<<Form("Pi+=%0.3f\%	Pi-=%0.3f	p/pbar=%0.3f	mu=%0.3f",pip/total,pim/total,p/total,mu/total)<<endl;
-
+  unsigned efficiency=0;
   for (int i = 0; i < signalTree->GetEntries(); ++i)
   {
-  	signalTree->GetEvent(i);
-  	s_deta->Fill(deta);
-  	s_layer->Fill(layer);
-  	s_prob->Fill(cluster_prob);
+  	if (cluster_prob<kPurityCutProb)
+  	{
+  		signalTree->GetEvent(i);
+	  	s_deta->Fill(deta);
+	  	s_layer->Fill(layer);
+	  	s_prob->Fill(cluster_prob);
+	  	efficiency++;
+  	}
   }
   b_deta->Scale(1/b_deta->Integral());
   b_layer->Scale(1/b_layer->Integral());
@@ -127,4 +141,6 @@ void backgroundPlotter()
   out->Close();
   delete backTree;
   delete out;
+  std::cout<<Form("efficiency:%.03f rejection:%0.3f",efficiency/(float)signalTree->GetEntries(),rejection/total)<<endl;
+
 }
