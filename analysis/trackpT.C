d4c1f7744ae7bd7fb21906c23d2837d1fb72d976
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-01-22 18:35:53 -0500

    first pT dist

--- analysis/trackpT.C
+++ analysis/trackpT.C
@@ -26,23 +26,23 @@ TChain* handleFile(string name, string extension, string treename, unsigned int 
   return all;
 }
 
-void cluster2Plotter()
+void trackpT()
 {
-  float eTpT[200];
-  float pTpT[200];
-  float eRpT[200];
-  float pRpT[200];
+  float electron_pt[200];
+  float positron_pt[200];
+  float electron_reco_pt[200];
+  float positron_reco_pt[200];
   int    truth_n;
 
   string treePath = "/sphenix/user/vassalli/gammasample/fourembededonlineanalysis";
   string treeExtension = ".root";
   unsigned int nFiles=100;
   TChain *ttree = handleFile(treePath,treeExtension,"ttree",nFiles);
-  ttree->SetBranchAddress("electron_pt",     &eTpT    );
-  ttree->SetBranchAddress("electron_reco_pt",&eRpT    );
-  ttree->SetBranchAddress("positron_pt",     &pTpT    );
-  ttree->SetBranchAddress("positron_reco_pt",&pRpT    );
-  ttree->SetBranchAddress("nTpair",          &truth_  );
+  ttree->SetBranchAddress("electron_pt",     &electron_pt    );
+  ttree->SetBranchAddress("electron_reco_pt",&electron_reco_pt    );
+  ttree->SetBranchAddress("positron_pt",     &positron_pt    );
+  ttree->SetBranchAddress("positron_reco_pt",&positron_reco_pt    );
+  ttree->SetBranchAddress("nTpair",          &truth_n  );
   
 
   string outfilename = "pTeffdists.root";
@@ -53,10 +53,10 @@ void cluster2Plotter()
   for (int event = 0; event < ttree->GetEntries(); ++event)
   {
     ttree->GetEvent(event);
-    for (int i = 0; i < nTpair; ++i)
+    for (int i = 0; i < truth_n; ++i)
     {
-      if (electron_reco_pt[nTpair]>0) h_TpT_NR->Fill(electron_pt[nTpair]);
-      if (positron_reco_pt[nTpair]>0) h_TpT_NR->Fill(positron_pt[nTpair]);
+      if (electron_reco_pt[truth_n]>0) h_TpT_NR->Fill(electron_pt[truth_n]);
+      if (positron_reco_pt[truth_n]>0) h_TpT_NR->Fill(positron_pt[truth_n]);
 
     }
   }  
