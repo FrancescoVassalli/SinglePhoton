c1c198a2187126595ae39adc18bbea9784017d9a
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-01-23 13:15:43 -0500

    resubmitted to fix seg fault

--- condor/buildTruth.bash
+++ condor/buildTruth.bash
@@ -2,8 +2,8 @@ cd ~/sphenix/single/truthconversion/build
 make install
 if [ $? -eq 0 ]; then
   cd ../../condor
-  if [ "$1" = "submit_cluster" ]; then
-    condor_submit submit_cluster
+  if [ "$1" = "submit_Cluster" ]; then
+    condor_submit submit_Cluster
   elif [ "$1" = "CondorClusterLocal.csh" ]; then
     CondorClusterLocal.csh
   fi
