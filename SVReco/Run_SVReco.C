02a971ec9f2ce810462f793c2d16f3fba1a18314
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-07-16 10:42:02 -0400

    initial from @sh-lim

---
+++ SVReco/Run_SVReco.C
@@ -0,0 +1,77 @@
+
+int Run_SVReco(
+		const int nEvents = 0,
+		//const char * inputFile = "G4sPHENIXCells_test.root",
+		const char * inputFile = "G4sPHENIX.root"
+		)
+{
+  //---------------
+  // Load libraries
+  //---------------
+
+  gSystem->Load("libfun4all.so");
+  gSystem->Load("libg4detectors.so");
+  gSystem->Load("libphhepmc.so");
+  gSystem->Load("libg4testbench.so");
+  gSystem->Load("libg4hough.so");
+  //gSystem->Load("libcemc.so");
+  gSystem->Load("libg4eval.so");
+	gSystem->Load("/gpfs/mnt/gpfs04/sphenix/user/shlim/02.MVTX/04.SVreco/test_20180617/SVReco/install/lib/libSVReco.so");
+
+  //---------------
+  // Fun4All server
+  //---------------
+
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+  // just if we set some flags somewhere in this macro
+  //recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You ca neither set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  // rc->set_IntFlag("RANDOMSEED", 12345);
+
+	SVReco *svreco = new SVReco();
+	svreco->set_do_eval(true);
+	svreco->set_cut_jet(true);
+	svreco->set_cut_jet_R(0.4);
+	svreco->set_cut_ncluster(false);
+	se->registerSubsystem( svreco );
+
+	Fun4AllInputManager *hitsin = new Fun4AllDstInputManager("DSTin");
+	hitsin->fileopen(inputFile);
+	se->registerInputManager(hitsin);
+
+  //-----------------
+  // Event processing
+  //-----------------
+	/*
+  if (nEvents < 0)
+    {
+      return;
+    }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !readhits && !readhepmc)
+    {
+      cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+      cout << "it will run forever, so I just return without running anything" << endl;
+      return;
+    }
+	*/
+
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+}
