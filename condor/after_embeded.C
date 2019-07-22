aabbe3c69d26ea02e7562e4dda0cf91c706805ff
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-07-22 15:08:04 -0400

    local runnning

--- condor/after_embeded.C
+++ condor/after_embeded.C
@@ -1,27 +1,28 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllDstInputManager.h>
 #include "TruthConversionEval.h"
+#include "RecoConversionEval.h"
 
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libtruthconversion.so)
-int after_embeded( std::string infile = "XjPhi3_pT5_98_dst.root",std::string outfile)
+int after_embeded( std::string infile = "XjPhi3_pT5_98_dst.root",std::string outfile="embedanalysis.root",int runNumber=0)
 {
   
   gSystem->Load("libfun4all.so");
   gSystem->Load("libg4detectors.so");
   gSystem->Load("libphhepmc.so");
   gSystem->Load("libg4testbench.so");
-  gSystem->Load("libg4hough.so");
+//  gSystem->Load("libg4hough.so");
 
   gSystem->Load("libg4eval.so");
   gSystem->Load("libcalotrigger.so");
-  gSystem->Load("libSinglePhotonAnalysis.so");
-  gSystem->Load("libConvertedPhotonReconstructor.so");
-  gSystem->Load("libtreemaker.so");
+  gSystem->Load("libtruthconversion.so");
+//  gSystem->Load("libConvertedPhotonReconstructor.so");
+ // gSystem->Load("libtreemaker.so");
 
-  int verbosity = 0;
+  int verbosity = 10;
   Fun4AllServer *se = Fun4AllServer::instance();
-  se->Verbosity(10);
+  se->Verbosity(verbosity);
   //recoConsts *rc = recoConsts::instance();
 
   Fun4AllInputManager *hitsin = new Fun4AllDstInputManager("DSTin");
@@ -39,4 +40,5 @@ int after_embeded( std::string infile = "XjPhi3_pT5_98_dst.root",std::string out
   std::cout << "All done" << std::endl;
 
   gSystem->Exit(0);
+  return 0;
 }
