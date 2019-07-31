4d703e8037cc4b4cf820072423f849e52d193189
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-07-31 16:56:16 -0400

    work on testing K cuts on pythoa background

--- analysis/closeBackAna.C
+++ analysis/closeBackAna.C
@@ -28,6 +28,8 @@ TChain* handleFile(string name, string extension, string treename, unsigned int 
 
 void plotGoodBack(TChain* ttree,TFile* out_file){
   float _bb_track_deta ;
+  float _bb_cluster_deta ;
+  float _bb_cluster_dphi ;
   float _bb_vtx_radius ;
   float _bb_track_dca ;
   int _bb_track_layer ;
@@ -43,7 +45,11 @@ void plotGoodBack(TChain* ttree,TFile* out_file){
   float _bb_track_dphi;
   int _bb_pid;
 
+  unsigned goodCount=0;
+
   ttree->Branch("track_deta", &_bb_track_deta);
+  ttree->Branch("cluster_deta", &_bb_cluster_deta);
+  ttree->Branch("cluster_dphi", &_bb_cluster_dphi);
   ttree->Branch("track_dca", &_bb_track_dca);
   ttree->Branch("track_dphi", &_bb_track_dphi);
   ttree->Branch("track_dlayer", &_bb_track_dlayer);
@@ -52,20 +58,20 @@ void plotGoodBack(TChain* ttree,TFile* out_file){
   ttree->Branch("vtx_radius", &_bb_vtx_radius);
   ttree->Branch("vtx_chi2", &_bb_vtx_chi2);
   ttree->Branch("approach_dist", &_bb_approach);
-  ttree->Branch("vtxTrackRZ_dist", &_bb_vtxTrackRZ_dist);
-  ttree->Branch("vtxTrackRPhi_dist", &_bb_vtxTrackRPhi_dist);
+  //ttree->Branch("vtxTrackRZ_dist", &_bb_vtxTrackRZ_dist);
+  //ttree->Branch("vtxTrackRPhi_dist", &_bb_vtxTrackRPhi_dist);
   ttree->Branch("photon_m", &_bb_photon_m);
   ttree->Branch("photon_pT", &_bb_photon_pT);
   ttree->Branch("cluster_prob", &_bb_cluster_prob);
-  ttree->Branch("pid", &_bb_pid);
+  //ttree->Branch("pid", &_bb_pid);
 
   std::vector<TH1F*> plots;
   plots.push_back(new TH1F("photon_m","",40,0,30));
   plots.push_back(new TH1F("photon_pT","",40,0,30));
   plots.push_back(new TH1F("vtx_radius","",40,0,30));
   plots.push_back(new TH1F("vtx_chi2","",40,0,30));
-  plots.push_back(new TH1F("vtxTrackRPhi_dist","",40,0,3));
-  plots.push_back(new TH1F("vtxTrackRZ_dist","",40,0,3));
+  //plots.push_back(new TH1F("vtxTrackRPhi_dist","",40,0,3));
+  //plots.push_back(new TH1F("vtxTrackRZ_dist","",40,0,3));
 
   for (std::vector<TH1F*>::iterator i = plots.begin(); i != plots.end(); ++i)
   {
@@ -76,7 +82,7 @@ void plotGoodBack(TChain* ttree,TFile* out_file){
   for (int event = 0; event < ttree->GetEntries(); ++event)
   {
     ttree->GetEvent(event);
-    if (_bb_track_layer>0&&_bb_track_pT>.6/*&&_bb_track_deta<.0082&&TMath::Abs(_bb_track_dlayer)<=2*/)
+    if (_bb_track_layer>0&&_bb_track_pT>.6&&_bb_track_dca>0&&-bb_cluster_prob>0&&_bb_track_deta<.3884&&TMath::Abs(_bb_track_dlayer)<=2)
     {
       plots[0]->Fill(_bb_photon_m);
       plots[1]->Fill(_bb_photon_pT);
