525c7b32002062927c7e6a71d46fc1fcf07824c4
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-08-01 10:23:33 -0600

    plotted photon_m refitdiffy is still weird

--- analysis/plotter.C
+++ analysis/plotter.C
@@ -10,15 +10,14 @@ void myText(Double_t x,Double_t y,Color_t color, const char *text, Double_t tsiz
 void photon_m(TFile* thisFile){
 	gStyle->SetOptStat(0);
 	std::vector<TH1F*> plots;
-	plots.push_back((TH1F*) thisFile->Get("m_{#gamma}_truth"));
-	plots.push_back((TH1F*) thisFile->Get("m_{#gamma}_reco"));
-	plots.push_back((TH1F*) thisFile->Get("m_{#gamma}_recoRefit"));
+	plots.push_back((TH1F*) thisFile->Get("m^{#gamma}_{recoRefit}"));
+	plots.push_back((TH1F*) thisFile->Get("m^{#gamma}_{reco}"));
 
 	TCanvas* tc = new TCanvas();
 	tc->Draw();
 	TLegend* tl = new TLegend(.7,.7,.9,.9);
-	plots[1]->SetLineColor(kRed);
-	plots[2]->SetLineColor(kGreen+2);
+	plots[0]->SetLineColor(kRed);
+	plots[1]->SetLineColor(kGreen+2);
 	for (int i = 0; i < plots.size(); ++i)
 	{
 		plots[i]->SetYTitle("#frac{dN}{dm *N_{#gamma}}");
@@ -34,8 +33,8 @@ void photon_m(TFile* thisFile){
 void recoRefit(TFile* thisFile){
 	gStyle->SetOptStat(0);
 	std::vector<TH1F*> plots;
-	plots.push_back((TH1F*) thisFile->Get("x_{0}-x_{refit}"));
 	plots.push_back((TH1F*) thisFile->Get("y_{0}-y_{refit}"));
+	plots.push_back((TH1F*) thisFile->Get("x_{0}-x_{refit}"));
 	plots.push_back((TH1F*) thisFile->Get("z_{0}-z_{refit}"));
 
 	TCanvas* tc = new TCanvas();
