273b88edf07420a10faec7c38e9ce804940e6fc3
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-08-12 18:30:37 -0400

    grp1

--- condor/FullMinBiasRecoJobs.pl
+++ condor/FullMinBiasRecoJobs.pl
@@ -2,7 +2,7 @@
 #inspired by @sh-lim
 use Cwd;
 use File::Copy qw(copy);
-
+#
 #$package = "pp200GeV_inelastic";
 #$package = "pp200GeV_hardqcd";
 #$package = "pp200GeV_softqcd";
@@ -12,40 +12,41 @@ use File::Copy qw(copy);
 $package = "minBiasConversion";
 $maindir = "/sphenix/user/vassalli";
 $logpath = "/direct/phenix+u/vassalli/sphenix/single/condor_logs/";
-$driver = "CondorMinBiasPythia.csh";
-
-$groupnum = 1;
-
+$driver = "CondorAfterMinBiasPythia.csh";
+#
+$groupnum = 0;
+#
 $rundir = "${maindir}/${package}/grp${groupnum}";
 mkdir $rundir;
-
-for ($irun=0; $irun<100; $irun++){
-  $wait =int(rand(55)); 
+#
+for ($irun=17; $irun<40; $irun++){
+  $wait =int(rand(5)); 
   print "Wait=${wait}\n";
   sleep $wait;
-
-	$wrkdir = "${rundir}_${irun}";
-	mkdir $wrkdir;
+  #
+  $wrkdir = "${rundir}_${irun}";
+  mkdir $wrkdir;
   copy $driver, $wrkdir;
   chdir $wrkdir;
-  chmod 777, $driver or die;
-	open(FILE, ">condor");
-	print FILE "Universe = vanilla\n";
-	print FILE "Notification = Never\n";
-	print FILE "Arguments = \$(Process) ${wrkdir}/\n";
-	print FILE "Requirements = CPU_Speed>=1\n";
-	print FILE "Rank = CPU_Speed\n";
-	print FILE "Priority = +2\n";
-	print FILE "Executable = $driver\n";
-	print FILE "Log = ${logpath}log.auto${irun}.\$(Process)\n";
-	print FILE "Output = ${logpath}out.auto${irun}.\$(Process)\n";
-	print FILE "Error = ${logpath}err.auto${irun}.\$(Process)\n";
+  chmod 0777, $driver or die;
+  open(FILE, ">condor");
+  print FILE "Universe = vanilla\n";
+  print FILE "Notification = Never\n";
+  print FILE "Arguments = \$(Process) ${wrkdir}/\n";
+  print FILE "Requirements = CPU_Speed>=1\n";
+  print FILE "Rank = CPU_Speed\n";
+  print FILE "Priority = +2\n";
+  print FILE "run_as_owner = True\n";
+  print FILE "Executable = $driver\n";
+  print FILE "Log = ${logpath}log.auto${irun}.\$(Process)\n";
+  print FILE "Output = ${logpath}out.auto${irun}.\$(Process)\n";
+  print FILE "Error = ${logpath}err.auto${irun}.\$(Process)\n";
   print FILE "Notify_user = frva5829\@colorado.edu\n";
-	print FILE "request_memory = 5000M\n";
-#	print FILE "+Experiment = \"phenix\"\n";
-#	print FILE "+Job_Type = \"cas\"\n";
-	print FILE "Queue 100\n";
-	close(FILE);
-	system "condor_submit condor";
+  print FILE "request_memory = 3000M\n";
+  # print FILE "+Experiment = \"phenix\"\n";
+  # print FILE "+Job_Type = \"cas\"\n";
+  print FILE "Queue 100\n";
+  close(FILE);
+  system "condor_submit condor";
 }
 
